bf7e1fb22376be016757963e1c8f66e72d26dc00
Christian Colglazier <christian@cacolglazier.com>
2019-06-19 15:22:13 -0400

    Monitor hack

--- i3/i3wm-config-gen.sh
+++ i3/i3wm-config-gen.sh
@@ -4,5 +4,3 @@
 . ~/.config/i3/settings.conf
 
 cat ~/.config/i3/shared.conf ~/.config/i3/${computer}.conf > ~/.config/i3/config
-
-i3-msg restart
---
+++ screenlayout/main.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 6000x1288 --rotate normal --output HDMI-0 --off --output DP-5 --off --output DP-4 --mode 3840x2160 --pos 2160x1288 --rotate normal --output DP-3 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off --output DP-0 --off
