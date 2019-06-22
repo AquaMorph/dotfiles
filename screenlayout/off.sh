1fe307a689f872518d4aeb068d8a3d9b1625a819
Christian Colglazier <christian@cacolglazier.com>
2019-06-21 20:10:28 -0400

    Removed monitor hack

--- screenlayout/off.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 6000x1288 --rotate normal --output HDMI-0 --off --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
