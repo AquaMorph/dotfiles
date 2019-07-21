34cea0c7c826aa5ebdfdfa55652fb45ee529ae54
Christian Colglazier <christian@cacolglazier.com>
2019-07-21 16:30:30 -0400

    Connecting to NAS

--- i3/i3wm-config-gen.sh
+++ i3/i3wm-config-gen.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # Load user settings from config file.
-. ~/.config/i3/settings.conf
+. ~/.config/settings.conf
 
 cat ~/.config/i3/shared.conf ~/.config/i3/${computer}.conf > ~/.config/i3/config
