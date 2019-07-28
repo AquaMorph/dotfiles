7ac78a5b587181888c87848d1b6b808beb90d429
Christian Colglazier <christian@cacolglazier.com>
2019-07-28 11:59:19 -0400

    Start up system audio script

---
+++ scripts/system-start-audio.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Kill Pulse
+pulseaudio -k
+
+# Start up jack
+cadence-session-start --system-start
+ladish_control sload Working7-27-19
+
+# Dumb hack to make audio setup right
+for i in {1..10}
+do
+    pulseaudio -k
+    sleep 0.1
+    pulseaudio -D
+done
+
+# Start up music player
+sleep 20 && i3-msg 'workspace 10; exec google-play-music-desktop-player'
