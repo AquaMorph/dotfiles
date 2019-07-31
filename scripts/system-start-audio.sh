f42f59f80a25d44624f5ba912754d5a889529b73
Christian Colglazier <christian@cacolglazier.com>
2019-07-31 09:50:59 -0400

    Startup ES-8 interface

--- scripts/system-start-audio.sh
+++ scripts/system-start-audio.sh
@@ -4,16 +4,21 @@
 pulseaudio -k
 
 # Start up jack
-cadence-session-start --system-start
-ladish_control sload Working7-27-19
+cadence-session-start --system-start &
+wait %1
+ladish_control sload Working7-29-19
 
 # Dumb hack to make audio setup right
-for i in {1..10}
+for i in {1..8}
 do
     pulseaudio -k
     sleep 0.1
     pulseaudio -D
 done
+
+# Start up eurorack interface
+alsa_in -d hw:1 &
+alsa_out -d hw:1 &
 
 # Start up music player
 sleep .1 && i3-msg 'workspace 10; exec google-play-music-desktop-player'
