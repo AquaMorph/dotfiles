fa93d2fe9d8ab48f367082b617da53a153256bde
Christian Colglazier <christian@cacolglazier.com>
2020-10-25 20:26:28 -0400

    Basic synth power CLI

--- scripts/audio/synth-power.py
+++ scripts/audio/synth-power.py
@@ -1,15 +1,18 @@
-
 #!/usr/bin/env python3
 
-# Program to control synth power state.
+# Program to control synthesizers power state.
+
 import argparse
 import configparser
+import os,sys,inspect
+currentDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentDir = os.path.dirname(currentDir)
+sys.path.insert(0, parentDir) 
 from homeassistant import HomeAssistant
-import os, sys
 
 # Parse settings config
 SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
-configString = '[Settings]\n' + open(SCRIPT_DIR + '/../settings.conf').read()
+configString = '[Settings]\n' + open(SCRIPT_DIR + '/../../settings.conf').read()
 configParser = configparser.RawConfigParser()
 configParser.read_string(configString)
 
@@ -41,5 +44,18 @@ def setSynthsPower(state):
     setHydrasynthPower(state)
     setMatrixBrutePower(state)
 
+parser = argparse.ArgumentParser(description='Control power state of synthesizers.')
+parser.add_argument('-d', '--daw', action='store_true',
+                    help='enable DAW mode',
+                    dest='daw', default=False, required=False)
+parser.add_argument('-o', '--off', action='store_true',
+                    help='turn all synths off',
+                    dest='off', default=False, required=False)
+args = parser.parse_args()
+
 ha = HomeAssistant(HA_IP, HA_TOKEN)
-setSynthsPower(False)
+
+if args.daw:
+    setSynthsPower(True)
+elif args.off:
+    setSynthsPower(False)
