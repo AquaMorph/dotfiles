880052d80ab4d91e797619ff9833f78aa26b83ae
Christian Colglazier <christian@cacolglazier.com>
2021-02-21 17:16:25 -0500

    Fixed Bitwig DSP window triggering automation

--- scripts/desktop/i3wm-close-window.py
+++ scripts/desktop/i3wm-close-window.py
@@ -4,8 +4,11 @@ import i3ipc, os
 
 def onWindowClose(conn, win):
     if(win.ipc_data['container']['window_properties']['class'] == 'Bitwig Studio'):
-        os.system('sh ~/.config/scripts/audio/aquamix.sh -n')
-        os.system('sh ~/.config/scripts/audio/synth-power-prompt.sh')
+        print(win.ipc_data['container']['window_properties']['title'])
+        if(win.ipc_data['container']['window_properties']['title'] !=
+           'DSP Performance Graph'):
+            os.system('sh ~/.config/scripts/audio/aquamix.sh -n')
+            os.system('sh ~/.config/scripts/audio/synth-power-prompt.sh')
 
 i3 = i3ipc.Connection()
 i3.on('window::close', onWindowClose)
