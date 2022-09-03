b61a1cc1922d95d380a2aae910b41caf88ba87ca
Christian Colglazier <christian@cacolglazier.com>
2022-09-02 20:26:55 -0400

    Change polybar desktop

--- scripts/desktop/polybar-start.sh
+++ scripts/desktop/polybar-start.sh
@@ -5,7 +5,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
 for monitor in $(xrandr -q | grep -e "\sconnected\s" | cut -d' ' -f1); do
-    if [ $monitor == 'DVI-D-0' ] || [ $monitor == 'LVDS-1' ]; then
+    if [ $monitor == 'DP-2' ] || [ $monitor == 'LVDS-1' ]; then
 	MONITOR=$monitor polybar aqua &
     fi
 done
