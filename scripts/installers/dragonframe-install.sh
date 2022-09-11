af38abbff2d96588b54753a27fdd446fd7a7e560
Christian Colglazier <christian@cacolglazier.com>
2022-09-11 13:48:30 -0400

    Dragonframe User-Agent

--- scripts/installers/dragonframe-install.sh
+++ scripts/installers/dragonframe-install.sh
@@ -7,7 +7,9 @@ source $(dirname ${BASH_SOURCE[0]})/install-lib.sh
 
 dragonframe=$(searchProgramInstalled dragonframe | awk 'END {print $(NF-2), $(NF-1), $NF}')
 dragonframeVersion=$(echo $dragonframe | awk '{print $2;}' | filterVersion)
-url=$(curl -s https://www.dragonframe.com/downloads/ | grep .rpm | grep downloadButton | grep -Po '(?<=href=")[^"]*.rpm')
+url=$(curl -s https://www.dragonframe.com/downloads/ \
+	   -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0" | \
+	  grep .rpm | grep downloadButton | grep -Po '(?<=href=")[^"]*.rpm')
 urlVersion=$(echo $url | awk -F "-" '{ print $2 }')
 
 # Check if installed to the most recent version
