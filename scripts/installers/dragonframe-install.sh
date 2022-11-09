f319f26fcb2e01e308992db3ad9ad5ad911ee0da
Christian Colglazier <christian@cacolglazier.com>
2022-11-09 07:59:53 -0500

    Fix Dragonframe download url parser

--- scripts/installers/dragonframe-install.sh
+++ scripts/installers/dragonframe-install.sh
@@ -9,7 +9,7 @@ dragonframe=$(searchProgramInstalled dragonframe | awk 'END {print $(NF-2), $(NF
 dragonframeVersion=$(echo $dragonframe | awk '{print $2;}' | filterVersion)
 url=$(curl -s https://www.dragonframe.com/downloads/ \
 	   -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0" | \
-	  grep .rpm | grep downloadButton | grep -Po '(?<=href=")[^"]*.rpm')
+	  grep .rpm | grep downloadButton | grep downloadButton | grep -io 'https://[a-z0-9+-._/]*.rpm')
 urlVersion=$(echo $url | awk -F "-" '{ print $2 }')
 
 # Check if installed to the most recent version
