c789c73630014b42f974dcc2a50634b720689153
Christian Colglazier <christian@cacolglazier.com>
2023-04-25 16:56:12 -0400

    Update mouse battery device name

--- scripts/get-mouse-battery.sh
+++ scripts/get-mouse-battery.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-mouseState=$(upower -i /org/freedesktop/UPower/devices/mouse_hidpp_battery_0)
+mouseState=$(upower -i /org/freedesktop/UPower/devices/battery_hidpp_battery_0)
 batteryPercentage=$(echo $mouseState | grep percentage | grep -Po '\d+%')
 charging=$(echo $mouseState | grep 'state: charging')
 
