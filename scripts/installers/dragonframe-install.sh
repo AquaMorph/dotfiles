e80a2ed715e5446f43c42512eaf864ffb5e6408a
Christian Colglazier <christian@cacolglazier.com>
2023-05-13 15:48:46 -0400

    Select newest version of Dragonframe

--- scripts/installers/dragonframe-install.sh
+++ scripts/installers/dragonframe-install.sh
@@ -5,11 +5,13 @@
 # Import library
 source $(dirname ${BASH_SOURCE[0]})/install-lib.sh
 
-dragonframe=$(searchProgramInstalled dragonframe | awk 'END {print $(NF-2), $(NF-1), $NF}')
+dragonframe=$(searchProgramInstalled dragonframe | \
+		  awk 'END {print $(NF-2), $(NF-1), $NF}')
 dragonframeVersion=$(echo $dragonframe | awk '{print $2;}' | filterVersion)
 url=$(curl -s https://www.dragonframe.com/downloads/ \
 	   -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0" | \
-	  grep .rpm | grep downloadButton | grep downloadButton | grep -io 'https://[a-z0-9+-._/]*.rpm')
+	  grep .rpm | grep downloadButton | grep downloadButton | \
+	  grep -io 'https://[a-z0-9+-._/]*.rpm' | head -n 1)
 urlVersion=$(echo $url | awk -F "-" '{ print $2 }')
 
 # Check if installed to the most recent version
@@ -22,5 +24,5 @@ cd ~/Downloads/installers/dragonframe
 wget $url
 
 # Install package
-sudo dnf install $(basename $url) -y
+sudo dnf install $(basename "$url") -y
 
